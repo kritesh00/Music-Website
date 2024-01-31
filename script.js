5aaa73c1df7d53463bdaf16cbe38dafdc6c2606a
kritesh00 <kriteshbhattarai617@gmail.com>
2024-01-30 20:51:18 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let mySong = document.getElementById("mySong");
+let icon = document.getElementById("icon");
+
+icon.onclick = function(){
+    if(mySong.paused){
+        mySong.play();
+        icon.src = "media/pause.png";
+    }
+    else{
+        mySong.pause();
+        icon.src = "media/play.png";
+    }
+}
